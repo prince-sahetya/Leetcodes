92555ad059ef4bca8ff333c143f208d65d609648
prince-sahetya <princesahetya@gmail.com>
2026-06-26 02:17:50 +0530

    Time: 7 ms (19.14%), Space: 10 MB (11.21%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -2,10 +2,19 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         if(s.size()!=t.size()) return false;
-    
-       sort(s.begin(),s.end());
-       sort(t.begin(),t.end());
-       if(s==t) return true ;
-       return false;
+        unordered_map<char,int>mpp;
+        for(int i=0;i<s.size();i++){
+            mpp[s[i]]++;
+        }
+        for(int i=0;i<t.size();i++){
+            if(mpp.find(t[i])==mpp.end()) return false;
+            mpp[t[i]]--;
+        }
+        for(int i=0;i<s.size();i++){
+            if(mpp[s[i]]!=0) return false;
+
+        }
+        return true;
+       
     }
 };
