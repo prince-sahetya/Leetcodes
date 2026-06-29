3235011eb5c42857e930012b4e1267746307907e
prince-sahetya <princesahetya@gmail.com>
2026-06-29 20:29:11 +0530

    Time: 0 ms (100%), Space: 8.1 MB (99.34%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int hammingWeight(int n) {
+        int count;
+        while(n!=0){
+            if(n&1){
+                count++;
+            }
+            n=n>>1;
+        }
+        return count;
+        
+    }
+};
