da1bd0e549851cb10d8dbce4dd1626c0d34b4bdb
prince-sahetya <princesahetya@gmail.com>
2026-07-14 00:58:14 +0530

    Time: 67 ms (32.51%), Space: 111.4 MB (7.92%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size()-1;i++){
-            if(nums[i]==nums[i+1]) return true;
+        unordered_map<int,int>mpp;
+        for(int i=0;i<nums.size();i++){
+            if(mpp.find(nums[i])!=mpp.end()) return true;
+            mpp[nums[i]]++;
         }
         return false;
     }
