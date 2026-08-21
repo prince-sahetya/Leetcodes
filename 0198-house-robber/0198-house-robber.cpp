6c656b4ac6ccd86682b1b9091f7565f64f107df0
prince-sahetya <princesahetya@gmail.com>
2026-08-21 12:47:38 +0530

    Time: 0 ms (100%), Space: 10.6 MB (65.12%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -9,8 +9,7 @@ public:
         vector<int>dp(nums.size(),-1);
         dp[0]=nums[0];
         if(nums.size()>=2)dp[1]=max(nums[0],nums[1]);
-        int ans = max(f(nums.size()-1,nums,dp),f(nums.size()-2,nums,dp));
-        for(auto it: dp) cout<<it<<" ";
+        int ans = f(nums.size()-1,nums,dp);
         return ans;
 
         
